6214cb3f31193aaa2a2278f3267df7c7b7267b24
ty730 <55007080+ty730@users.noreply.github.com>
2021-04-15 23:56:38 -0700

    Added serial prints to blinkLed

--- arduino/blinkLed.ino
+++ arduino/blinkLed.ino
@@ -1,13 +1,17 @@
 const int LED_OUTPUT_PIN = 3;
+const int BAUD_RATE = 9600;
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_OUTPUT_PIN, OUTPUT);
+  Serial.begin(BAUD_RATE);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(LED_OUTPUT_PIN, HIGH); // turn LED on (output 5V)
+  Serial.println("Pin 3 is HIGH (5V)");
   delay(500); // wait half a second
   digitalWrite(LED_OUTPUT_PIN, LOW); // turn LED off (output 0V)
+  Serial.println("Pin 3 is LOW (0V)");
   delay(500); // wait half a second
 }
